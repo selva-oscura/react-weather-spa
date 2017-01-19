6093a0109ffbc3976ca1c2bddccad2e4a06f1810
Carol St.Louis <1.selva.oscura@gmail.com>
2017-01-18 20:55:20 -0800

    added state to App; verified state data available;

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Header from './Header';
 import Body from './Body';
 import '../styles/App.css';
 
-class App extends Component {
+const App = React.createClass({
+	getInitialState(){
+		return {
+			currPage: "splash",
+			currLocation: {
+				city: "",
+				country: "",
+			},
+			favedLocations: [],
+			searchInput: {
+				city: "",
+				country: "",
+			},
+			error: "",
+		}
+	},
   render() {
     return (
       <div className="App">
         <Header />
         <Body />
+        <p>{this.state.currPage}</p>
       </div>
     );
   }
-}
+})
 
 export default App;
