47e24544def23215e4abe455d2097bf5f23c9077
Carol St.Louis <1.selva.oscura@gmail.com>
2017-01-18 18:33:01 -0800

    imported and displayed Header

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Header from './Header';
 import '../styles/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <p>hello world</p>
       </div>
     );
