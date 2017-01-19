48795464c6e6996f068a0fd3c99f7d7e22752739
Carol St.Louis <1.selva.oscura@gmail.com>
2017-01-18 22:13:25 -0800

    removed console.log

--- src/components/NavLink.js
+++ src/components/NavLink.js
@@ -3,7 +3,6 @@ import React from 'react';
 const NavLink = ({pageLink, handleNav}) => {
 	let display;
 	display = pageLink[0];
-	console.log("id", pageLink)
 	return (
 			<div 
 				className="NavLink"
