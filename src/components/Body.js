83991d3fdf71b58319964241985b1acac3c57949
Carol St.Louis <1.selva.oscura@gmail.com>
2017-01-18 22:00:32 -0800

    added switch statement to render different content, dependent on parameter passed from App (currPage)

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,40 @@
 import React from 'react';
 import '../styles/Body.css';
 
-const Body = () => (
-	<section className="Body">
-		<p>hello body</p>
-	</section>
-)
+const Body = ({currPage}) => {
+	let page;
+	switch(currPage){
+		case "splash":
+			page = (
+				<h3>splash</h3>
+			); 
+			break;
+		case "detail":
+			page = (
+				<h3>detail</h3>
+			);
+			break;
+		case "favorites":
+			page = (
+				<h3>favorites</h3>
+			); 
+			break;
+		case "settings":
+			page = (
+				<h3>settings</h3>
+			); 
+			break;
+		default:
+			page = (
+				<h3>default</h3>
+			);
+			break;
+	}
+	return (
+		<section className="Body">
+			{page}
+		</section>
+	)
+};
 
 export default Body;
