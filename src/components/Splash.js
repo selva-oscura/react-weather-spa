8eaf5c0b3cbe208aa146ce950ec1a1b72a39f994
Carol St.Louis <1.selva.oscura@gmail.com>
2017-01-18 22:21:52 -0800

    created stub for Splash Component (body for splash / landing page)

---
+++ src/components/Splash.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Splash = () => (
+	<div
+		className="Splash"
+	>
+		<h2>Splash</h2>
+		<p>will be the landing/splash page, perhaps weather images???</p>
+	</div>
+);
+
+export default Splash;
