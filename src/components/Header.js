486add34908aae0aa7fb65263a5c5bf61db57bef
Carol St.Louis <1.selva.oscura@gmail.com>
2017-01-18 18:32:48 -0800

    created Header Component stub

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = () => (
+	<header>
+		<div>
+			<p>search</p>
+		</div>
+		<div>
+			<p>faves</p>
+		</div>
+		<div>
+			<p>preferences</p>
+		</div>
+	</header>
+)
+
+export default Header;
