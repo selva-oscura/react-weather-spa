8711a51d4f346f3c9b41a1f2f800cbc70b88d017
Carol St.Louis <1.selva.oscura@gmail.com>
2017-01-18 22:20:39 -0800

    created stub for Favorites Component (list of favorited/saved locations)

---
+++ src/components/Favorites.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Favorites = () => (
+	<div
+		className="Favorites"
+	>
+		<h2>Favorites</h2>
+		<p>will be a list of favorited locations with basic today's forecast and link to details page for each location</p>
+	</div>
+);
+
+export default Favorites;
