c5836b48ada96312e4a21b61b6471c5377dd2414
Carol St.Louis <1.selva.oscura@gmail.com>
2017-01-18 22:21:13 -0800

    created stub for Settings page (e.g. C/F prefs)

---
+++ src/components/Settings.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Settings = () => (
+	<div
+		className="Settings"
+	>
+		<h2>Settings</h2>
+		<p>will have settings options, e.g. Fahrenheit versus Celsius</p>
+	</div>
+);
+
+export default Settings;
