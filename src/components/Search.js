d0004b5d0f1499dd81bba97bac6a50dde33340e6
Carol St.Louis <1.selva.oscura@gmail.com>
2017-01-20 21:42:09 -0800

    imported countries and console logged first entry

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,18 @@
 import React from 'react';
-
+import countries from '../resources/countries.json';
+console.log('countries', countries[0]);
 
 const Search = () => {
+	let options = countries.map((country) => {
+		return (
+			<option 
+				key={country.Code}
+				value={country.Code}
+			>
+				{country.Name}
+			</option>
+		)
+	});
 	return (
 		<form 
 			className="Search"
