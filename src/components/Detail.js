085f3ff0d938e7dbb2aeb924db221913faa12d2d
Carol St.Louis <1.selva.oscura@gmail.com>
2017-01-21 20:33:27 -0800

    added currLocation as parameter; displayed selected location in text;

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Detail = () => (
+const Detail = ({currLocation}) => (
 	<div
 		className="Detail"
 	>
 		<h2>Detail</h2>
-		<p>Detail view of forecast</p>
+		<p>Checking weather conditions for {currLocation.city[0].toUpperCase() + currLocation.city.slice(1).toLowerCase()}, {currLocation.country}.</p>
 	</div>
 );
 
