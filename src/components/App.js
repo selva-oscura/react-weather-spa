00abdb6ce23d808056042e8076d5d2bef7edb8ce
Carol St.Louis <1.selva.oscura@gmail.com>
2017-01-22 22:25:02 -0800

    added sample weather data to state for passing via Body to Detail to mock data in Detail

--- src/components/App.js
+++ src/components/App.js
@@ -9,8 +9,55 @@ console.log('openWeather', openWeather)
 const App = React.createClass({
 	getInitialState(){
 		return {
-			currPage: "splash",
-			currLocation: {},
+			currPage: "detail",
+			currLocation: {
+				city: "Washington, DC",
+				country: "US",
+				apiResponse: {
+					"coord":{
+						"lon":-77.03,
+						"lat":38.89
+					},
+					"sys":{
+						"message":0.1888,
+						"country":"United States",
+						"sunrise":1485174027,
+						"sunset":1485209995
+					},
+					"weather":[
+						{
+							"id":500,
+							"main":"Rain",
+							"description":"light rain",
+							"icon":"10n"
+						}
+					],
+					"base":"stations",
+					"main":{
+						"temp":9.927,
+						"temp_min":9.927,
+						"temp_max":9.927,
+						"pressure":1010.12,
+						"sea_level":1016.62,
+						"grnd_level":1010.12,
+						"humidity":92
+					},
+					"wind":{
+						"speed":2.97,
+						"deg":40.5034
+					},
+					"clouds":{
+						"all":92
+					},
+					"rain":{
+						"3h":0.565
+					},
+					"dt":1485134447,
+					"id":0,
+					"name":"Washington",
+					"cod":200
+				}
+			},
 			favedLocations: [],
 			searchInput: {
 				city: "",
@@ -40,27 +87,48 @@ const App = React.createClass({
 		e.preventDefault();
 		let {errors, currPage, searchInput} = this.state;
 		if(searchInput.city.length>0){
+			errors = [];
 			let currLocation={};
 			currLocation = {
 				city: searchInput.city,
 				country: searchInput.country,
 			}
-			if(searchInput.id){
-				currLocation.id = searchInput.id;
-			}
+			searchInput.id ? currLocation.id = searchInput.id : currLocation.id=null;
 			currPage="detail";
-			errors = [];
-			this.setState({errors, currPage, currLocation});
-			this.queryWeatherAPI();
+			this.setState({errors, currLocation, currPage});
+			this.queryWeatherAPI(currLocation);
 		}else{
 			errors = ["Please enter a city (and state/province if needed)."];
 			this.setState({errors});
 		}
 	},
-	queryWeatherAPI(){
-		let {city, country, id} = this.state.currLocation;
-		console.log(`single location query for ${city}, ${country} to go here`);
-		console.log(id, "check");
+	purgeOfSpacesAndCommas(str){
+		let response = [];
+		str.split("").forEach((char, i) => {
+			let charcode = char.charCodeAt(0);
+			if((charcode>=65&&charcode<=90)
+				|| (charcode>=97&&charcode<=122)){
+				response.push(char);
+			}
+			else if(char===" "){
+				response.push("+");
+			}else if(char===","){
+				response.push("%2C");
+			}
+		});
+		return response.join("");
+	},
+	queryWeatherAPI(currLocation){
+		let {city, country, id} = currLocation;
+		let tempFormat = this.state.settings.tempFormat;
+		let apiCall=`${openWeather.baseURL}weather/`;
+		if(id){
+			apiCall += `city?id=${id}`;
+		}else{
+			apiCall += `?q=${this.purgeOfSpacesAndCommas(city)},${country}`
+		}
+		apiCall += `&appid=${openWeather.apiKey}&units=${tempFormat}`;
+		console.log('apiCall', apiCall);
 	},
 	queriesWeatherAPI(){
 		console.log('multi-location query to go here');
