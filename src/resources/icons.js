94534acae7f0cb0e98bdf14c85c4feddb7700519
Carol St.Louis <1.selva.oscura@gmail.com>
2017-01-22 22:20:50 -0800

    created import file for openWeather images

---
+++ src/resources/icons.js
@@ -0,0 +1,36 @@
+import icon01d from '../../public/images/01d.png';
+import icon01n from '../../public/images/01n.png';
+import icon02d from '../../public/images/02d.png';
+import icon02n from '../../public/images/02n.png';
+import icon03d from '../../public/images/03d.png';
+import icon03n from '../../public/images/03n.png';
+import icon04d from '../../public/images/04d.png';
+import icon04n from '../../public/images/04n.png';
+import icon09d from '../../public/images/09d.png';
+import icon09n from '../../public/images/09n.png';
+import icon10d from '../../public/images/10d.png';
+import icon10n from '../../public/images/10n.png';
+import icon13d from '../../public/images/13d.png';
+import icon13n from '../../public/images/13n.png';
+import icon50d from '../../public/images/50d.png';
+import icon50n from '../../public/images/50n.png';
+
+let icons = {
+	icon01d,
+	icon01n,
+	icon02d,
+	icon02n,
+	icon03d,
+	icon03n,
+	icon04d,
+	icon04n,
+	icon09d,
+	icon09n,
+	icon10d,
+	icon10n,
+	icon13d,
+	icon13n,
+	icon50d,
+	icon50n,
+}
+export default icons;
