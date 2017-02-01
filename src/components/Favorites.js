4fc931655997d90d536e48117193adfef1c25d4a
Carol St.Louis <1.selva.oscura@gmail.com>
2017-02-01 14:50:16 -0800

    calculated style for current-temperature and DayForecast to be proportional to number of days; added timeData variable to shorten moment.js specifications;

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -17,33 +17,36 @@ const Favorite = ({locationId, favedLocation, tempFormat}) => {
 		// path to url for icon
 	const localAddress = (iconCode) => ( icons[`icon${iconCode}`] );
 
-	// calculate min/max temperatures for each day
+	// calculate & store min/max temperatures and day name for each day
 	let days = [], 
 		hi = -Infinity, 
 		lo = Infinity, 
 		lastIndex = favedLocation.data.length-1, 
-		dayName = moment.utc(favedLocation.data[0].dt*1000).tz(favedLocation.zoneName).format("ddd"),
-		dayOffset = moment.utc(favedLocation.data[0].dt*1000).tz(favedLocation.zoneName).format("DDDD");
+		timeData = moment.utc(favedLocation.data[0].dt*1000).tz(favedLocation.zoneName),
+		dayName = timeData.format("ddd"),
+		dayNumber = timeData.format("DDDD"),
+		dayOffset = timeData.format("DDDD");
 	favedLocation.data.forEach((timestamp, i) => {
-		let dayNumber = moment.utc(timestamp.dt*1000).tz(favedLocation.zoneName).format("DDDD");
-		console.log('dayNumber', dayNumber, 'dayOffset', dayOffset);
+		timeData = moment.utc(timestamp.dt*1000).tz(favedLocation.zoneName);
+		dayNumber = timeData.format("DDDD");
 		if(dayOffset !== dayNumber){
-			console.log('should be pushing')
 			days.push({dayName: dayName, hi: hi, lo: lo});
-			dayName = moment.utc(timestamp.dt*1000).tz(favedLocation.zoneName).format("ddd")
 			hi = -Infinity
 			lo = Infinity;
 			dayOffset = dayNumber;
 		}
+		dayName = timeData.format("ddd");
 		hi = Math.max(Math.round(timestamp.main.temp), hi);
 		lo = Math.min(Math.round(timestamp.main.temp), lo);
-		if(i === lastIndex && moment.utc(timestamp.dt*1000).tz(favedLocation.zoneName).format("HH")>20){
+		if(i === lastIndex && timeData.format("HH")>20){
 			days.push({dayName: dayName, hi: hi, lo: lo});
 		}
 	});
-	days.forEach((day)=>{console.log(JSON.stringify([].concat(day)))})
 	days.shift();
-	days.forEach((day)=>{console.log(JSON.stringify([].concat(day)))})
+	let forecastStyle = {
+		width: `${Math.floor(95/(days.length+1))}%`
+	}
+
 	return(
 		<div className="Favorite">
 			<div className="city-info">
@@ -57,7 +60,10 @@ const Favorite = ({locationId, favedLocation, tempFormat}) => {
 				</p>
 			</div>
 			<div className="weather-info">
-				<div className="current-temperature">
+				<div 
+					className="current-temperature"
+					style={forecastStyle}
+				>
 					<h3>{Math.round(favedLocation.data[0].main.temp)}&deg;{tempFormat==="metric" ? "C" : "F"}</h3>
 					<img src={localAddress(favedLocation.data[0].weather[0].icon)} alt={favedLocation.data[0].weather[0].description}/>  
 				</div>
@@ -77,15 +83,15 @@ const Favorite = ({locationId, favedLocation, tempFormat}) => {
 };
 
 const DayForecast = ({day, dayCount, tempFormat}) => {
-	let DayForeCastStyle = {
-		width: `${Math.floor(100/(dayCount+1))}%`
+	let forecastStyle = {
+		width: `${Math.floor(95/(dayCount+1))}%`
 	}
 	return (
 		<div 
 			className="DayForecast" 
-			style={DayForeCastStyle}
+			style={forecastStyle}
 		>
-			<p>{day.dayName}</p>
+			<h3>{day.dayName}</h3>
 			<p>{day.hi}&deg;{tempFormat==="metric" ? "C" : "F"}</p>
 			<p>{day.lo}&deg;{tempFormat==="metric" ? "C" : "F"}</p>
 		</div>
