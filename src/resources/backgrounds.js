9b6ba541b5a32ac19bb54c900d714135fc713507
Carol St.Louis <1.selva.oscura@gmail.com>
2017-02-06 15:19:15 -0800

    import file for all background images

---
+++ src/resources/backgrounds.js
@@ -0,0 +1,20 @@
+import background01l from '../../public/images/backgrounds/01l.jpg';
+import background01p from '../../public/images/backgrounds/01p.jpg';
+import background02l from '../../public/images/backgrounds/02l.jpg';
+import background02p from '../../public/images/backgrounds/02p.jpg';
+import background03l from '../../public/images/backgrounds/03l.jpg';
+import background03p from '../../public/images/backgrounds/03p.jpg';
+import background04l from '../../public/images/backgrounds/04l.jpg';
+import background04p from '../../public/images/backgrounds/04p.jpg';
+
+let backgrounds = {
+	background01l,
+	background01p,
+	background02l,
+	background02p,
+	background03l,
+	background03p,
+	background04l,
+	background04p,
+}
+export default backgrounds;
